dee090ec58272b55f7d55a25bf4f843efdce831d
dNanda777 <71mikoshiba@gmail.com>
2020-04-06 21:24:40 +0800

    changed title

--- addNewResidence.php
+++ addNewResidence.php
@@ -19,35 +19,44 @@
 
                     <div class="form-group">
                         <label for="Address">address</label>
-                        <input type="text" name="address" class="form-control col-md-7" placeholder="e.g. Block A">
+                        <input type="text" name="address" class="form-control
+												 col-md-7" placeholder="e.g. Block A">
                     </div>
 
                     <div class="form-group">
                         <label for="Size">size</label>
-                        <input type="number" name="size" class="form-control col-md-7" placeholder="e.g. 1 acre">
+                        <input type="number" name="size" class="form-control
+												 col-md-7" placeholder="e.g. 1 acre">
                     </div>
 
                     <div class="form-group">
                         <label for="Bedrooms">bedrooms</label>
-                        <input type="number" name="bedroomNum" class="form-control col-md-7" placeholder="Number of bedrooms">
+                        <input type="number" name="bedroomNum" class="
+												form-control col-md-7" placeholder="Number of bedrooms">
                     </div>
 
 										<div class="form-group">
                         <label for="Kitchens">kitchens</label>
-                        <input type="number" name="kitchenNum" class="form-control col-md-7" placeholder="Number of kitchens">
+                        <input type="number" name="kitchenNum" class="
+												form-control col-md-7" placeholder="Number of kitchens">
                     </div>
 
 										<div class="form-group">
                         <label for="Bathrooms">bathrooms</label>
-                        <input type="number" name="bathroomNum" class="form-control col-md-7" placeholder="Number of bathrooms">
+                        <input type="number" name="bathroomNum" class=
+												"form-control col-md-7" placeholder="
+												Number of bathrooms">
                     </div>
 
 										<div class="form-group">
                         <label for="Monthly Rental">monthlyRental</label>
-                        <input type="number" name="monthlyRental" class="form-control col-md-7" placeholder="Price of rental per month">
+                        <input type="number" name="monthlyRental" class="
+												form-control col-md-7" placeholder="Price of rental
+												per month">
                     </div>
 
-                    <button type="submit" class="btn btn-primary" name="save">SAVE</button>
+                    <button type="submit" class="btn btn-primary" name="save">
+											SAVE</button>
                     <button type="reset" class="btn btn-danger">CANCEL</button>
                 </form>
 
@@ -77,6 +86,7 @@
 
             echo "<div align='center'><h6> Loading... </h6></div>";
             //echo "<h5>DONE</h5>";
-            echo "<meta http-equiv='refresh' content='1;url=http://localhost/mhb/tes_tabel_residences.php'>";
+            echo "<meta http-equiv='refresh' content='1;url=
+						http://localhost/mhb/tes_tabel_residences.php'>";
         }
 ?>
